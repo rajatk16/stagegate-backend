bd307a3216100cb17ae4ab8fb40d6b2d0ccc7b64
sudagar <rajat.sudagade@autodesk.com>
2025-10-18 15:46:07 +0530

    chore: update package dependencies and add GraphQL code generation scripts

---
+++ codegen.ts
@@ -0,0 +1,13 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'src/graphql/typeDefs.ts',
+  generates: {
+    'src/graphql/types.ts': {
+      plugins: ['typescript', 'typescript-resolvers'],
+    },
+  },
+};
+
+export default config;
