f19d567a2424c3e213c44d2638396590108095c8
sudagar <rajat.sudagade@autodesk.com>
2026-01-22 21:56:57 +0530

    feat: organization events query

--- src/graphql/resolvers/event/event.resolver.ts
+++ src/graphql/resolvers/event/event.resolver.ts
@@ -1,15 +1,19 @@
 import { Resolvers } from '../../types';
 import { createEvent } from './mutations';
+import { organizationEvents } from './query';
 import { members, organization, viewerEventRole, viewerOrgRole } from './fieldResolvers';
 
 export const resolvers: Resolvers = {
-  Mutation: {
-    createEvent,
-  },
   Event: {
     members,
     organization,
     viewerOrgRole,
     viewerEventRole,
+  },
+  Query: {
+    organizationEvents,
+  },
+  Mutation: {
+    createEvent,
   },
 };
